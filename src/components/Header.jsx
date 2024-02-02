6bb0724edc65c8dad3c4bd94a1fb57de8ea7d504
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-02-03 00:28:59 +0100

    ...

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>IP Address Tracker</h1>
+            <div>
+                <input type="search" name="search" id="search" />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
