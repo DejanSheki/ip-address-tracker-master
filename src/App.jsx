736d9d6cd3ee209a39a762427bec6a5b4f429d6b
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-02-04 14:03:12 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Header from './components/Header';
 function App() {
 
   return (
-    <>
+    <main className='max-w-[1440px] m-auto'>
       <Header />
-    </>
+    </main>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+
+
 const Header = () => {
     return (
-        <header>
+        <header className="bg-[url('/pattern-bg-desktop.png')] bg-no-repeat bg-cover bg-center h-[40vh] flex flex-col justify-center items-center">
             <h1>IP Address Tracker</h1>
             <div>
                 <input type="search" name="search" id="search" />
