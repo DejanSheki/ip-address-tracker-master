df2e2acf833392602eefb177e5628a96c67e592f
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-02-08 22:02:00 +0100

    ...

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -3,17 +3,10 @@ import { useState, useEffect } from "react";
 import Info from './Info';
 import Map from './Map';
 
-import { useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 const Data = () => {
 
     const [clientsData, setClientsData] = useState([]);
-
-    const mapRef = useRef(null);
-    // const latitude = 51.505;
-    // const longitude = -0.09;
 
     const [latitude, setLatitude] = useState(51.505);
     const [longitude, setLongitude] = useState(-0.09)
@@ -34,19 +27,7 @@ const Data = () => {
     return (
         <div className="w-[1440px] h-[75vh] flex-wrap relative flex flex-col">
             <Info infoData={clientsData} />
-            <div className="w-[1440px] h-[75vh] flex flex-wrap relative z-0">
-                <MapContainer
-                    center={[latitude, longitude]}
-                    ref={mapRef}
-                    zoom={13}
-                    style={{ height: "100%", width: "100%" }}
-                >
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                </MapContainer>
-            </div>
+            <Map mapCentre={[latitude, longitude]} />
         </div>
     )
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,14 @@
-import { useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const Map = () => {
-  const mapRef = useRef(null);
+function MapComponent(props) {
+  const map = useMap();
+  console.log("map center:", map.getCenter());
+  map.panTo(props.mapCentre);
+  return null;
+}
+
+const Map = (props) => {
   const latitude = 51.505;
   const longitude = -0.09;
 
@@ -11,10 +16,10 @@ const Map = () => {
     <div className="w-[1440px] h-[75vh] flex flex-wrap relative z-0">
       <MapContainer
         center={[latitude, longitude]}
-        ref={mapRef}
         zoom={13}
         style={{ height: "100%", width: "100%" }}
       >
+        <MapComponent mapCentre={props.mapCentre} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
