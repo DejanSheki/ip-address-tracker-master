ca945cb2622252ac7d5605943da2abc1f5af52ef
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-02-10 22:20:16 +0100

    update search....

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import Header from './components/Header';
 import Data from './components/FetchData';
+
+
 function App() {
 
   return (
-    <main className='max-w-[1440px] m-auto '>
-      <Header />
-      <Data />
-    </main>
+    <Data />
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { MdKeyboardArrowRight } from "react-icons/md";
 
-const Header = () => {
+const Header = ({ onSearch, searchValue, onClick, onKeyDown }) => {
+
     return (
         <header className="bg-[url('/pattern-bg-mobile.png')] md:bg-[url('/pattern-bg-desktop.png')] bg-no-repeat bg-cover bg-center h-[33vh] md:h-[30vh] flex flex-col justify-start items-center gap-7 pt-8">
             <h1 className="text-white font-medium text-2xl md:text-4xl tracking-normal">IP Address Tracker</h1>
@@ -10,11 +11,19 @@ const Header = () => {
                     type="text"
                     placeholder="Search for any IP address or domain"
                     id="search"
+                    onChange={onSearch}
+                    value={searchValue}
+                    onKeyDown={onKeyDown}
                 />
-                <button className="bg-black text-white cursor-pointer px-4 py-[0.92rem] rounded-e-xl hover:bg-[rgba(43,43,43,0.9)]"><MdKeyboardArrowRight size={26} /></button>
+                <button
+                    className="bg-black text-white cursor-pointer px-4 py-[0.92rem] rounded-e-xl hover:bg-[rgba(43,43,43,0.9)]"
+                    onClick={onClick}
+                ><MdKeyboardArrowRight size={26} /></button>
             </div>
         </header>
     )
 }
 
+
 export default Header;
+
